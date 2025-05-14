5162ddd628a30eabd80c2db12d2cc20e19ae72ee
muzea <mr.muzea@gmail.com>
2025-05-14 13:48:01 +0800

    feat: env/config 解析参数

--- packages/mcp/src/args.ts
+++ packages/mcp/src/args.ts
@@ -2,29 +2,29 @@ import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import { AgentConfig } from '@tiny-tool/browser/agent';
 
-const args = yargs(hideBin(process.argv))
-  .option('type', {
-    alias: 't',
-    type: 'string',
-    default: 'stdio',
-    choices: ['sse', 'stdio'],
-    description: 'Specify the type (sse/stdio) of server',
-  })
-  .option('port', {
-    alias: 'p',
-    type: 'number',
-    default: 3000,
-    description: 'Specify the port for the server',
-  })
-  .option('disableHeadless', {
-    type: 'boolean',
-    default: false,
-  })
-  .parseSync();
-
-export default args;
-
 export function parseArgs() {
+  const args = yargs(hideBin(process.argv))
+    .env('browser_mcp')
+    .config()
+    .option('type', {
+      alias: 't',
+      type: 'string',
+      default: 'stdio',
+      choices: ['sse', 'stdio', 'streamable'],
+      description: 'Specify the type (sse/stdio) of server',
+    })
+    .option('port', {
+      alias: 'p',
+      type: 'number',
+      default: 3000,
+      description: 'Specify the port for the server',
+    })
+    .option('disableHeadless', {
+      type: 'boolean',
+      default: false,
+    })
+    .parseSync();
+
   const agentConfig: AgentConfig = {
     headless: true,
   };
